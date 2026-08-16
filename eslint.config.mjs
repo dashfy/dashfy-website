0adc0ac704f45336c70f6f8d8af466a9296f1381
brenopolanski <breno.polanski@gmail.com>
2026-08-16 10:19:21 -0300

    feat(eslint): integrate Prettier into ESLint configuration for better formatting support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import unusedImports from 'eslint-plugin-unused-imports'
 
@@ -8,6 +9,7 @@ import unusedImports from 'eslint-plugin-unused-imports'
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   globalIgnores([
     '.next/**',
     '.source/**',
